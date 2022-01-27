fcf1024196ee88e5f3434f221375b2116c1a8fa3
Sumit <sumitdhiman2001@gmail.com>
2022-01-27 15:05:55 +0530

    changed some of the files

--- combination.c
+++ combination.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 int main(){
-    int number, answer, enter;
+    int number, answer, enter, i;
     printf("Press 1 to calculate Equivalent Resistance in for Series combination.\n");
     printf("Press 2 to calculate Equivalent Resistance in for Parallel combination.\n");
     scanf("%d", &enter);
     printf("Enter the number of resitances: ");
-    scanf("%d", number);
+    scanf("%d\n", number);
       if (enter==1)
       {
+          for ( i = 0; i < number; i++)
+          {
+             printf("Enter the value of R%d", i+1); 
+             scanf("%d", );
+             
+
+          }
           
 
       }
